84fa299706d1e068dcdad983fcf1e43a2acff460
37HAN <83692187+37HAN@users.noreply.github.com>
2021-05-05 10:16:15 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', message => {
+	if (message.content === `${prefix}ping`) {
+		message.channel.send('Ping Deez Nutz!!!');
+	} else if (message.content === `${prefix}beep`) {
+		message.channel.send('Beep Deez Buttz!!!');
+	} else if (message.content === `${prefix}server`) {
+		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+	} else if (message.content === `${prefix}user-info`) {
+		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+	}
+});
+
+client.login(token);
